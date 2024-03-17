ff5a7802dc8dcf7fb84767f758864fe35b05cd1f
Ian Day <ian@ianday.me>
2024-03-17 17:06:18 -0400

    logging

--- backend/plant_tracker_api/main.py
+++ backend/plant_tracker_api/main.py
@@ -15,7 +15,28 @@ from logging.config import dictConfig
 import logging
 from config import LogConfig
 
-dictConfig(LogConfig().dict())
+dictConfig(
+    config={
+        # Logging config
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": "%(levelprefix)s | %(asctime)s | %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "default": {
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stderr",
+            },
+        },
+        "loggers": {"mycoolapp": {"handlers": ["default"], "level": "DEBUG"}},
+    }
+)
 logger = logging.getLogger("mycoolapp")
 
 
